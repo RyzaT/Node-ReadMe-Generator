adca514990a95da97fb7190b7b0a3c986cffbde6
RyzaT <ryantaylor50@hotmail.com>
2023-02-17 15:38:06 +0000

    added questions

--- starter/index.js
+++ starter/index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const { default: Choices } = require("inquirer/lib/objects/choices");
 
 // Title H1
 // Description
@@ -18,13 +19,62 @@ const questions = [
     {
         type: "input",
         name: "title",
-        message: "What's the title of your project?",
+        message: "Hi! What's the title of your project?",
     },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project",
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Please select a licence applicable to this project",
+        choices: ["MIT", "Apache 2.0", "Boost 1.0", "MPL 2.0", "BSD2", "BSD3", "None"],
+    },
+    {
+        type: "input",
+        name: "require",
+        message: "List any requirements or dependancies here",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "List any languages or technologies associated within this project",
+    },
+    {
+        type: "input",
+        name: "creator",
+        message: "What'your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "What's your full name?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter a valid email address",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Please list any contributors",
+        default: "",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide a details of any tests if applicable"
+    },
+
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
