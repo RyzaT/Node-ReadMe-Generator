da9c5cfa05c7cd40cbc70098998677242998f633
RyzaT <ryantaylor50@hotmail.com>
2023-02-17 16:17:48 +0000

    added licensing bit's (i hope)

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,4 +1,27 @@
-// function to generate markdown for README
+// function to return license badge
+function licenseBadge(licence) {
+  if (licence !== "none") {
+    return `![Github license](https://img.shields.io/badge/license-${licence}-yellowgreen.svg)`;
+  }
+  return "";
+}
+// function to return license link
+function licenceLink(licence) {
+  if (licence !== "none") {
+    return `/n* [License](#license)/n`;
+  }
+  return "";
+}
+// function to return the license section to readme
+function licenseSection(licence) {
+  if (licence !== "none") {
+    return `## License
+      Licensed under the ${licence} license.`;
+  }
+  return "";
+}
+
+\// function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
