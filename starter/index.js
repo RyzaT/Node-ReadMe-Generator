625db6b3abc168b20feaf8bb36dea9f33577144c
RyzaT <ryantaylor50@hotmail.com>
2023-02-17 14:11:26 +0000

    Added starter code and psuedo

---
+++ starter/index.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// Title H1
+// Description
+// Table of Contents
+// Installation
+// Usage
+// License
+// Contributing
+// Tests
+// Questions
+
+// array of questions for user
+const questions = [
+    type: "input",
+    name: "title",
+    
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
